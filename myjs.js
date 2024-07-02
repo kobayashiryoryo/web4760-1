7fe6d5bfd72f726b74703f596707e9146264c629
kobayashiryoryo <167833061+kobayashiryoryo@users.noreply.github.com>
2024-07-02 10:25:58 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -2,8 +2,8 @@ alert('Hello,Ryota!');
 alert(1+1);
 alert(Math.sqrt(1+1));
 alert(Math.sqrt(2+3));
-alert(1/0);
-alert(0/0);
+alert(1÷0);
+alert(0÷0);
 alert(2+3);
 alert('2'+'3');
 alert(Number('2')+Number('3')
